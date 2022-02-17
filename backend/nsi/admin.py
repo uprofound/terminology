7e7a63df253a6d17897dbd0d6c4378f95136ad24
uprofound <uzunitem@yandex.ru>
2022-02-17 12:56:18 +0300

    Settings for admin-zone

--- backend/nsi/admin.py
+++ backend/nsi/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 
 from .models import Catalog, CatalogContent, CatalogVersion
 
-admin.site.register(Catalog)
-admin.site.register(CatalogVersion)
-admin.site.register(CatalogContent)
+
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'version_count')
+    list_filter = ('id', 'short_name')
+
+    def version_count(self, obj):
+        return CatalogVersion.objects.filter(catalog=obj).count()
+
+
+class CatalogVersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'catalog', 'version', 'start_date')
+    list_filter = ('catalog', 'version', 'start_date')
+
+
+class CatalogContentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'catalog_version', 'code', 'value')
+    list_filter = ('catalog_version', 'code', 'value')
+
+
+admin.site.register(Catalog, CatalogAdmin)
+admin.site.register(CatalogVersion, CatalogVersionAdmin)
+admin.site.register(CatalogContent, CatalogContentAdmin)
